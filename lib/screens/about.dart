6cccaa8aeb9892eefd2831b8f310f21bcbdb4660
naytzap <jonaaas@live.com>
2022-10-18 16:56:59 +0200

    added url launcher to about page repo source

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class About extends StatelessWidget {
   const About({super.key});
+  static const String repoUrl = "github.com/naytzap/gipfelbuch";
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(Uri.parse("https://$repoUrl"),mode: LaunchMode.externalApplication)) {
+      throw 'Could not launch $repoUrl';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +21,8 @@ class About extends StatelessWidget {
         alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Card(
+          children:  <Widget>[
+            const Card(
                 child: ListTile(
                   leading: Icon(Icons.people),
                   title: Text("Authors"),
@@ -23,13 +31,13 @@ class About extends StatelessWidget {
             ),
             Card(
               child: ListTile(
-                //Todo: url launcher
-                leading: Icon(Icons.code),
-                title: Text("Source"),
-                subtitle: Text("github.com/naytzap/gipfelbuch\nLicense: GNU General Public License v3.0"),
+                leading: const Icon(Icons.code),
+                title: const Text("Source"),
+                subtitle: const Text("$repoUrl\nLicense: GNU General Public License v3.0"),
+                onTap: _launchUrl,
               )
             ),
-            Expanded(
+            const Expanded(
               child: Center(
               child: Text("Du, Berg, bist gut. Auf deinen Matten ruht\nDas Auge gern und gern auf deinem Wald;\nDu bist nicht hoch und stattlich von Gestalt,\nDoch macht dein sanfter Reiz dem Träumer Mut.\nDie Sonne liegt auf deiner breiten Brust\nDen langen Tag; du gibst sie uns zurück;\nUnd über deinem gütevollen Glück\nEntlässt das Herz die letzte böse Lust.\n(Christian Morgenstern)",
                     textAlign: TextAlign.center,
@@ -37,7 +45,7 @@ class About extends StatelessWidget {
                     ),
             ),
             ),
-            Image(image: AssetImage('assets/11_Langkofel_group_Dolomites_Italy.jpg')),
+            const Image(image: AssetImage('assets/11_Langkofel_group_Dolomites_Italy.jpg')),
           ],
         ),
       ),
